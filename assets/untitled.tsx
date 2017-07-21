4907d4278ca2ce2ed4ab5d0e2c0e26874953a123
Tiago Lisboa <tiago.caio.ol@gmail.com>
2017-07-21 17:18:22 -0300

    Por fim consegui adaptar o algoritimo A* para a linguagem potigol.
    Foi uma luta de titans para ver quem conhecia menos os conceitos
    basicos de programação, eu ou o interpretador. Um evento tão caótico
    que fez com que eu desistisse de todos os padrões adotados para
    esse projeto, e buscasse apenas a finalização do mesmo e a ruina
    de certas pessoas. Por isso aqui esta escrito o registro do que
    aconteceu nessa faze, e não em uma série de bonitos commitinhos,
    mas em um grande commit de merda.
    
    Porém, no ápice do meu estrese, finalmente descobri como burlar
    as gambiarras e bugs do compilador e, com minha própria
    gambiarra, fiz com que a orrenda função retornasse o valor
    correto e com que o demoniaco enquanto saisse do loop infinito
    injustificavel.
    
    Infelizmente minha luta não está no fim, o jogo ainda não está
    terminado e os bugs da linguágem ainda não foram corrigidos.
    
    Você, leitor, que possivelmente se encontra nas mesmas terras
    de desgraça e ruina que quem uma vez escreveu estas linhas, está
    completamente fudido.
    
    Você está fudido desde que foi designado a trabalhar com essa
    ferramenta. Você não terá materiais de consulta, nem pessoas
    a quem perguntar. Deverá descobrir como fazer o que quer por
    conta própria, o que quer dizer que vai ter que adivinhar.
    
    Não lhe desejo boa sorte pois você está fudido e não tem como
    voltar atrás, então desista.
    
    Bjs, o commiteiro.

---
+++ assets/untitled.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tileset" tilewidth="32" tileheight="32" tilecount="4" columns="4">
+ <image source="../../../../../../gameDev-shit/melonjs/topdown/data/img/map/tileset.png" width="128" height="32"/>
+</tileset>
